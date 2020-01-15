c7b1f6d4c8241e19d507c1d237251ef2171cb38a
mtondolo <mtondolo@gmail.com>
2020-01-15 10:00:59 +0200

    Change listview to recyclerview

--- app/src/main/java/com/example/android/bookvillage/CatalogActivity.java
+++ app/src/main/java/com/example/android/bookvillage/CatalogActivity.java
@@ -4,11 +4,16 @@ import android.content.ContentUris;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import androidx.loader.app.LoaderManager;
 import androidx.loader.content.CursorLoader;
 import androidx.loader.content.Loader;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -37,63 +42,67 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate ( savedInstanceState );
-        setContentView ( R.layout.activity_catalog );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
 
         // Setup FAB to open EditorActivity
-        FloatingActionButton fab = (FloatingActionButton) findViewById ( R.id.fab );
-        fab.setOnClickListener ( new View.OnClickListener () {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent ( CatalogActivity.this, EditorActivity.class );
-                startActivity ( intent );
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                startActivity(intent);
             }
-        } );
+        });
 
         // Find the ListView which will be populated with the book data
-        ListView bookListView = (ListView) findViewById ( R.id.list );
+        /*ListView bookListView = (ListView) findViewById(R.id.list_books);*/
 
-        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
-        View emptyView = findViewById ( R.id.empty_view );
-        bookListView.setEmptyView ( emptyView );
+        RecyclerView recyclerBooks = findViewById(R.id.list_books);
+        LinearLayoutManager booksLinearLayoutManager = new LinearLayoutManager(this);
+        recyclerBooks.setLayoutManager(booksLinearLayoutManager);
+        
+       /* // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        bookListView.setEmptyView(emptyView);*/
 
         // Setup an Adapter to create a list item for each row of book data in the Cursor.
         // There is no book data yet (until the loader finishes) so pass in null for the Cursor.
-        mCursorAdapter = new BookCursorAdapter ( this, null );
-        bookListView.setAdapter ( mCursorAdapter );
+      /*  mCursorAdapter = new BookCursorAdapter(this, null);
+        bookListView.setAdapter(mCursorAdapter);
 
         // Setup the item click listener
-        bookListView.setOnItemClickListener ( new AdapterView.OnItemClickListener () {
+        bookListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 // Create new intent to go to {@link EditorActivity}
-                Intent intent = new Intent ( CatalogActivity.this, EditorActivity.class );
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
 
                 // Form the content URI that represents the specific book that was clicked on,
                 // by appending the "id" (passed as input to this method) onto the
                 // {@link BookEntry#CONTENT_URI}.
                 // For example, the URI would be "content://com.example.android.bookvillage/books/2"
                 // if the book with ID 2 was clicked on.
-                Uri currentBookUri = ContentUris.withAppendedId ( BookEntry.CONTENT_URI, id );
+                Uri currentBookUri = ContentUris.withAppendedId(BookEntry.CONTENT_URI, id);
 
                 // Set the URI on the data field of the intent
-                intent.setData ( currentBookUri );
+                intent.setData(currentBookUri);
 
                 // Launch the {@link EditorActivity} to display the data for the current book.
-                startActivity ( intent );
+                startActivity(intent);
             }
-        } );
+        });*/
 
         // Kick off the loader
-        getSupportLoaderManager ().initLoader ( BOOK_LOADER, null, this );
+        getSupportLoaderManager().initLoader(BOOK_LOADER, null, this);
     }
 
     /**
      * Helper method to delete all books in the database.
      */
     private void deleteAllBooks() {
-        int rowsDeleted = getContentResolver ().delete ( BookEntry.CONTENT_URI, null, null );
-        Log.v ( "CatalogActivity", rowsDeleted + " rows deleted from book database" );
+        int rowsDeleted = getContentResolver().delete(BookEntry.CONTENT_URI, null, null);
+        Log.v("CatalogActivity", rowsDeleted + " rows deleted from book database");
     }
 
     @Override
@@ -101,20 +110,20 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
 
         // Inflate the menu options from the res/menu/menu_catalog.xml file.
         // This adds menu items to the app bar.
-        getMenuInflater ().inflate ( R.menu.menu_catalog, menu );
-        return super.onCreateOptionsMenu ( menu );
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // User clicked on a menu option in the app bar overflow menu
-        switch (item.getItemId ()) {
+        switch (item.getItemId()) {
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
-                deleteAllBooks ();
+                deleteAllBooks();
                 return true;
         }
-        return super.onOptionsItemSelected ( item );
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -128,25 +137,25 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
                 BookEntry.COLUMN_BOOK_PRICE};
 
         // This loader will execute the ContentProvider's query method on a background thread
-        return new CursorLoader( this,
+        return new CursorLoader(this,
                 BookEntry.CONTENT_URI,   // The table to query
                 projection,            // The columns to return
                 null,         // The columns for the WHERE clause
                 null,      // The values for the WHERE clause
-                null );
+                null);
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
 
         // Update {@link BookCursorAdapter} with this new cursor containing updated book data
-        mCursorAdapter.swapCursor ( data );
+        mCursorAdapter.swapCursor(data);
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
 
         // Callback called when the data needs to be deleted
-        mCursorAdapter.swapCursor ( null );
+        mCursorAdapter.swapCursor(null);
     }
 }
