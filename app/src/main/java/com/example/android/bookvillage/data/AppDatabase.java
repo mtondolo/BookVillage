c06b90bca9c79c8ca2c1accae32f345e9a0743fa
mtondolo <mtondolo@gmail.com>
2020-01-18 11:52:30 +0200

    Create room database

---
+++ app/src/main/java/com/example/android/bookvillage/data/AppDatabase.java
@@ -0,0 +1,29 @@
+package com.example.android.bookvillage.data;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+public abstract class AppDatabase extends RoomDatabase {
+    private static final String LOG_TAG = AppDatabase.class.getSimpleName();
+    private static final Object LOCK = new Object();
+    private static final String DATABASE_NAME = "bookvillage";
+    private static AppDatabase sInstance;
+
+    public static AppDatabase getInstance(Context context) {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                Log.d(LOG_TAG, "Creating new database instance");
+                sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                        AppDatabase.class, AppDatabase.DATABASE_NAME)
+                        .build();
+            }
+        }
+        Log.d(LOG_TAG, "Getting the database instance");
+        return sInstance;
+    }
+
+    public abstract BookDao bookDao();
+}
