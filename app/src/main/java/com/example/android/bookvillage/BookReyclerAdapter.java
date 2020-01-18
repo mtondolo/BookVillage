0a3ecf95c4f3b89208ed42a72c6d9da6285df71d
mtondolo <mtondolo@gmail.com>
2020-01-19 00:17:45 +0200

    Query all books in catalogactivity

--- app/src/main/java/com/example/android/bookvillage/BookReyclerAdapter.java
+++ app/src/main/java/com/example/android/bookvillage/BookReyclerAdapter.java
@@ -1,85 +1,122 @@
 package com.example.android.bookvillage;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.android.bookvillage.data.BookEntry;
+
+import java.util.List;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-/*
 public class BookReyclerAdapter extends RecyclerView.Adapter<BookReyclerAdapter.ViewHolder> {
+    private List<BookEntry> mBookEntries;
     private final Context mContext;
-    private final LayoutInflater mLayoutInflater;
-    private Cursor mCursor;
+    /* private Cursor mCursor;*/
     private final BookReyclerAdapterOnClickHandler mClickHandler;
 
     public interface BookReyclerAdapterOnClickHandler {
         void onClick(int _id);
     }
 
-    public BookReyclerAdapter(Context context, Cursor cursor, BookReyclerAdapterOnClickHandler clickHandler) {
+    /**
+     * Constructor for the BookRecyclerAdapter that initializes the Context.
+     *
+     * @param context      the current Context
+     * @param clickHandler the ItemClickListener
+     */
+    public BookReyclerAdapter(Context context, BookReyclerAdapterOnClickHandler clickHandler) {
         mContext = context;
-        mCursor = cursor;
         mClickHandler = clickHandler;
-        mLayoutInflater = LayoutInflater.from(mContext);
     }
 
+    /**
+     * Called when ViewHolders are created to fill a RecyclerView.
+     *
+     * @return A new BookViewHolder that holds the view for each task
+     */
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = mLayoutInflater.inflate(R.layout.item_book_list, parent, false);
+        View itemView = LayoutInflater.from(mContext).inflate(R.layout.item_book_list, parent, false);
         return new ViewHolder(itemView);
     }
 
+    /**
+     * Called by the RecyclerView to display data at a specified position in the Cursor.
+     *
+     * @param holder   The ViewHolder to bind Cursor data to
+     * @param position The position of the data in the Cursor
+     */
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        mCursor.moveToPosition(position);
+        //mCursor.moveToPosition(position);
+        BookEntry bookEntry = mBookEntries.get(position);
 
-        String quantity = mCursor.getString(CatalogActivity.INDEX_QUANTITY);
-        String name = mCursor.getString(CatalogActivity.INDEX_NAME);
-        String price = mCursor.getString(CatalogActivity.INDEX_PRICE);
+        // Determine the values of the wanted data
+        String name = bookEntry.getBook_name();
+        int quantity = bookEntry.getQuantity();
+        double price = bookEntry.getPrice();
 
-        holder.mTextQuantity.setText(quantity);
+        //Set values
         holder.mTextName.setText(name);
-        holder.mTextPrice.setText(price);
+        holder.mTextQuantity.setText(Integer.toString(quantity));
+        holder.mTextPrice.setText(Double.toString(price));
     }
 
+    /**
+     * Returns the number of items to display.
+     */
     @Override
     public int getItemCount() {
-        if (null == mCursor) return 0;
-        return mCursor.getCount();
+        if (mBookEntries == null) {
+            return 0;
+        }
+        return mBookEntries.size();
     }
 
-    void swapCursor(Cursor newCursor) {
-        mCursor = newCursor;
+    /**
+     * When data changes, this method updates the list of bookEntries
+     * and notifies the adapter to use the new values on it
+     */
+    public void setBooks(List<BookEntry> bookEntries) {
+        mBookEntries = bookEntries;
         notifyDataSetChanged();
     }
 
+
+   /* void swapCursor(Cursor newCursor) {
+        mCursor = newCursor;
+        notifyDataSetChanged();
+    }*/
+
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        private final TextView mTextQuantity;
         private final TextView mTextName;
+        private final TextView mTextQuantity;
         private final TextView mTextPrice;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            mTextQuantity = itemView.findViewById(R.id.text_quantity);
             mTextName = itemView.findViewById(R.id.text_name);
+            mTextQuantity = itemView.findViewById(R.id.text_quantity);
             mTextPrice = itemView.findViewById(R.id.text_price);
             itemView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View view) {
-            int adapterPosition = getAdapterPosition();
-            mCursor.moveToPosition(adapterPosition);
-            int bookId = mCursor.getInt(CatalogActivity.INDEX_ID);
-            mClickHandler.onClick(bookId);
+           /* int adapterPosition = getAdapterPosition();
+            //mCursor.moveToPosition(adapterPosition);
+            BookEntry bookEntry = mBookEntries.get(adapterPosition);
+            //int bookId = mCursor.getInt(CatalogActivity.INDEX_ID);
+            int bookId = bookEntry.getId();
+            mClickHandler.onClick(bookId);*/
         }
     }
 }
-*/
+
--- app/src/main/java/com/example/android/bookvillage/CatalogActivity.java
+++ app/src/main/java/com/example/android/bookvillage/CatalogActivity.java
@@ -1,20 +1,15 @@
 package com.example.android.bookvillage;
 
 import android.content.Intent;
-import android.database.Cursor;
 
+import com.example.android.bookvillage.data.AppDatabase;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import androidx.loader.app.LoaderManager;
-import androidx.loader.content.CursorLoader;
-import androidx.loader.content.Loader;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,39 +19,35 @@ import android.view.View;
 /**
  * Displays list of books that were entered and stored in the app.
  */
-/*
-public class CatalogActivity extends AppCompatActivity */
-/*implements
-        LoaderManager.LoaderCallbacks<Cursor>,
-        BookReyclerAdapter.BookReyclerAdapterOnClickHandler *//*
- {
 
-    */
+public class CatalogActivity extends AppCompatActivity implements BookReyclerAdapter.
+        BookReyclerAdapterOnClickHandler {
+
+
 /*public static final String[] MAIN_CATALOG_PROJECTION = {
             BookEntry._ID,
             BookEntry.COLUMN_BOOK_QUANTITY,
             BookEntry.COLUMN_BOOK_NAME,
             BookEntry.COLUMN_BOOK_PRICE,
-    };*//*
+    };*/
 
 
-   */
-/* public static final int INDEX_ID = 0;
+
+    /*public static final int INDEX_ID = 0;
     public static final int INDEX_QUANTITY = 1;
     public static final int INDEX_NAME = 2;
-    public static final int INDEX_PRICE = 3;*//*
+    public static final int INDEX_PRICE = 3;*/
 
 
-    */
-/* private static final int BOOK_LOADER = 0;*//*
+    //private static final int BOOK_LOADER = 0;
 
 
-    Cursor mCursor;
+    /*Cursor mCursor;*/
     private BookReyclerAdapter mBookReyclerAdapter;
     private RecyclerView mRecyclerBooks;
-    private View mEmptyView;
-    private int mPosition = RecyclerView.NO_POSITION;
-
+    /*private View mEmptyView;
+    private int mPosition = RecyclerView.NO_POSITION;*/
+    private AppDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,35 +65,46 @@ public class CatalogActivity extends AppCompatActivity */
         });
 
         mRecyclerBooks = findViewById(R.id.list_books);
-        mEmptyView = findViewById(R.id.empty_view);
+        //mEmptyView = findViewById(R.id.empty_view);
 
         LinearLayoutManager booksLinearLayoutManager = new LinearLayoutManager(this);
         mRecyclerBooks.setLayoutManager(booksLinearLayoutManager);
 
-        // mBookReyclerAdapter = new BookReyclerAdapter(this, mCursor, this);
+        mBookReyclerAdapter = new BookReyclerAdapter(this, this);
         mRecyclerBooks.setAdapter(mBookReyclerAdapter);
 
-        */
-/* LoaderManager.getInstance(this).initLoader(BOOK_LOADER, null, this);*//*
 
+        /*LoaderManager.getInstance(this).initLoader(BOOK_LOADER, null, this);*/
+        // Initialize member variable for the data base
+        mDb = AppDatabase.getInstance(getApplicationContext());
     }
 
-    private void showRecyclerBooks() {
+    /**
+     * This method is called after this activity has been paused or restarted.
+     * Often, this is after new data has been inserted through an EditorActivity,
+     * so this re-queries the database data for any changes.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //  Call the adapter's setBooKs method using the result
+        // of the loadAllTasks method from the taskDao
+        mBookReyclerAdapter.setBooks(mDb.bookDao().loadAllBooks());
+    }
+/* private void showRecyclerBooks() {
         mEmptyView.setVisibility(View.INVISIBLE);
         mRecyclerBooks.setVisibility(View.VISIBLE);
-    }
+    }*/
 
-    */
-/**
+    /**
      * Helper method to delete all books in the database.
-     *//*
+     */
 
-   */
-/* private void deleteAllBooks() {
+
+ /* private void deleteAllBooks() {
         int rowsDeleted = getContentResolver().delete(BookEntry.CONTENT_URI, null, null);
         Log.v("CatalogActivity", rowsDeleted + " rows deleted from book database");
-    }*//*
-
+    }*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu options from the res/menu/menu_catalog.xml file.
@@ -123,8 +125,16 @@ public class CatalogActivity extends AppCompatActivity */
         return super.onOptionsItemSelected(item);
     }
 
-  */
-/*  @Override
+    @Override
+    public void onClick(int _id) {
+        /*Intent editorActivityIntent = new Intent(CatalogActivity.this, EditorActivity.class);
+        Uri uriForBookClicked = BookEntry.buildBookUriWithId(_id);
+        editorActivityIntent.setData(uriForBookClicked);
+        startActivity(editorActivityIntent);*/
+    }
+
+
+/*@Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         // This loader will execute the ContentProvider's query method on a background thread
         return new CursorLoader(this,
@@ -133,36 +143,35 @@ public class CatalogActivity extends AppCompatActivity */
                 null,         // The columns for the WHERE clause
                 null,      // The values for the WHERE clause
                 null);
-    }*//*
+    }*/
 
 
-   */
-/* @Override
+
+  /*@Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
         // Update {@link BookCursorAdapter} with this new cursor containing updated book data
         mBookReyclerAdapter.swapCursor(data);
         if (mPosition == RecyclerView.NO_POSITION) mPosition = 0;
         mRecyclerBooks.smoothScrollToPosition(mPosition);
         if (data.getCount() != 0) showRecyclerBooks();
-    }*//*
+    }*/
 
 
-   */
-/* @Override
+
+ /*@Override
     public void onLoaderReset(Loader<Cursor> loader) {
         // Callback called when the data needs to be deleted
         mBookReyclerAdapter.swapCursor(null);
-    }*//*
+    }*/
 
 
-   */
-/* @Override
+  /*  @Override
     public void onClick(int _id) {
         Intent editorActivityIntent = new Intent(CatalogActivity.this, EditorActivity.class);
         Uri uriForBookClicked = BookEntry.buildBookUriWithId(_id);
         editorActivityIntent.setData(uriForBookClicked);
         startActivity(editorActivityIntent);
-    }*//*
+    }*/
 
 }
-*/
+
